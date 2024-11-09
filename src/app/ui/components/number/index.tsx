5524229c5df15d68b21ab50f76d9e29b487884f1
JoaoYamaguti <joaov.yamaguti@gmail.com>
2024-11-09 20:48:26 -0300

    fix something

--- src/app/ui/components/number/index.tsx
+++ src/app/ui/components/number/index.tsx
@@ -7,26 +7,25 @@ export default function Number({ label, min, max, setValue }: IOptionParams) {
 
     const [valid, setValid] = useState(true)
 
-    function handleValue (val: number){
-        if(min === undefined || max === undefined || val === undefined) return alert('')
-            if (val < min || val > max){
+    function handleValue(val: number) {
+        if (min === undefined || max === undefined || val === undefined) return alert('')
+        if (val < min || val > max) {
             setValid(false)
         } else {
             setValid(true)
-
         }
 
         setValue(val)
     }
 
     return (
-            <div className={styles.display}>
-                <span>{label}</span>
-                <div className={styles.number}>
-                    <span>min:{min}</span>
-                    <input className={valid ? styles.valid : styles.invalid} type="number" name="" id="" onChange={(e) => handleValue(parseFloat(e.target.value))} />
-                    <span>max:{max}</span>
-                </div>
+        <div className={styles.display}>
+            <span>{label}</span>
+            <div className={styles.number}>
+                <span>min:{min}</span>
+                <input className={valid ? styles.valid : styles.invalid} type="number" name="" id="" onChange={(e) => handleValue(parseFloat(e.target.value))} />
+                <span>max:{max}</span>
             </div>
+        </div>
     )
 }
