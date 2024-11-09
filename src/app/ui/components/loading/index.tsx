ddf4773ea750d10f6e7449c6f130bb7a39364f82
JoaoYamaguti <joaov.yamaguti@gmail.com>
2024-11-09 19:57:56 -0300

    responsivity in process and loading component

--- src/app/ui/components/loading/index.tsx
+++ src/app/ui/components/loading/index.tsx
@@ -1,10 +1,13 @@
 import styles from './style.module.scss'
 
-export default function Loading(){
+export default function Loading() {
     return (
-        <div className={styles.loading}>
-            <div></div>
-            <span>Loading...</span>
+        <div className={styles.container}>
+
+            <div className={styles.loading}>
+                <div></div>
+                <span>Loading...</span>
+            </div>
         </div>
     )
 }
--- src/app/ui/components/process/index.tsx
+++ src/app/ui/components/process/index.tsx
@@ -21,13 +21,13 @@ export default function Process({ setVisibility, process }: ProcessParams) {
 
     const [urlFile, setUrlFile] = useState('')
     const [urlNewFile, setUrlNewFile] = useState('')
-    
+
     useEffect(() => {
         try {
             if (process !== undefined) {
                 Magic(process, setUrlFile, setUrlNewFile)
             }
-            
+
         } catch (error) {
             console.log(error)
         }
@@ -35,59 +35,57 @@ export default function Process({ setVisibility, process }: ProcessParams) {
     }, [process])
 
     return (
-        <div className={styles.container} onClick={() => { setVisibility(false) }}>
-            <main onClick={(event) => event.stopPropagation()}>
-                {
-                    urlNewFile ? (
-                        <>
-                            <div className={styles.close} onClick={() => { setVisibility(false) }}></div>
-                            <div className={styles.content}>
-                                <section className={styles.response}>
-                                    <div className={styles.figures}>
-                                        <div>
-                                            <span>Imagem Antiga:</span>
-                                            {urlNewFile && <Image
-                                                src={urlFile}
-                                                alt={'old image'}
-                                                width={200}
-                                                height={200}
-                                            />}
-                                        </div>
-                                        <div>
-                                            <span>Imagem Nova:</span>
-                                            {urlNewFile && <Image
-                                                src={urlNewFile}
-                                                alt={'new image'}
-                                                width={200}
-                                                height={200}
-                                            />}
-                                        </div>
-
+        <>
+            {urlNewFile ? (
+                <div className={styles.process} onClick={() => { setVisibility(false) }}>
+                    <main onClick={(event) => event.stopPropagation()}>
+                        <div className={styles.close} onClick={() => { setVisibility(false) }}></div>
+                        <div className={styles.content}>
+                            <section className={styles.response}>
+                                <div className={styles.figures}>
+                                    <div>
+                                        <span>Atteched Image:</span>
+                                        {<Image
+                                            src={urlFile}
+                                            alt={'old image'}
+                                            width={240}
+                                            height={240}
+                                        />}
                                     </div>
 
-                                    <a href={urlNewFile} download>
-                                        <Image
-                                            src={'/download-direto-w.png'}
-                                            alt='dodnload'
-                                            width={15}
-                                            height={15}
-                                        />
-                                        Download</a>
-                                </section>
+                                    <div>
+                                        <span>New Image:</span>
+                                        {<Image
+                                            src={urlNewFile}
+                                            alt={'new image'}
+                                            width={240}
+                                            height={240}
+                                        />}
+                                    </div>
+                                </div>
 
-                                <hr />
+                                <a href={urlNewFile} download>
+                                    <Image
+                                        src={'/download-direto-w.png'}
+                                        alt='download'
+                                        width={25}
+                                        height={25}
+                                    />
+                                    Download New Image
+                                </a>
+                            </section>
 
-                                <section className={styles.login}>
-                                    <span>Gostaria de salvar suas imagens?</span>
-                                    <Link href={'/session/login'}> Login</Link>
-                                </section>
-                            </div>
-                        </>
-                    ) : (
-                        <Loading />
-                    )
-                }
-            </main>
-        </div>
+                            <hr />
+
+                            <section className={styles.login}>
+                                <span>Would you like save your images?</span>
+                                <Link href={'/session/login'}> Login or Sing In</Link>
+                            </section>
+                        </div>
+                    </main>
+                </div>
+            ) : (<Loading />)
+            }
+        </>
     )
 }
